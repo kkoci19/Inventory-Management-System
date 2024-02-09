fa6a4fb693e1daecfa77af2980027fff97b3d808
anxhi66 <anxheladauti37@gmail.com>
2024-02-09 16:53:39 +0100

    product request class

---
+++ src/main/java/Model/ProductRequest.java
@@ -0,0 +1,49 @@
+package Model;
+
+public class ProductRequest {
+    private String productName;
+    private String productDescription;
+    private Double price;
+    private int quantity;
+    private Category category;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
